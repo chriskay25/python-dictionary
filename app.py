7d432ecf7f2a02baeb8909a6d3f057180c0d183b
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-22 13:06:33 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import json
+
+data = json.load(open('data.json'))
+
+def translate(word):
+    if word in data:
+        return data[word]
+    else:
+        return 'Sorry, could not find that word. Please try again.'
+
+word = input('Enter word: ')
+
+print(translate(word))
