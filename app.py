d82c9f60dcef660d0b8df592b9779387ccbc2665
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-22 17:14:22 -0400

    Use difflib to check for words similar to user entry

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open('data.json'))
 
@@ -6,9 +7,18 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        confirm = input('Did you mean %s? Enter "y" for yes or "n" for no.' % get_close_matches(word, data.keys())[0])
+        confirm = confirm.lower()
+        if confirm == 'y':
+            return data[get_close_matches(word, data.keys())[0]]
+        elif confirm == 'n':
+            return 'Sorry, could not find that word. Please try again.'
+        else:
+            'Sorry, invalid confirmation.'
     else:
         return 'Sorry, could not find that word. Please try again.'
 
 word = input('Enter word: ')
 
-print(translate(word))
+print(translate(word)) 
